c44a6250503a767394e706ada0a6b95ab6d63304
Joshua Merritt <jjmerri88@gmail.com>
2020-02-16 07:00:47 -0500

    update name for case sensitivity

--- src/app/mod-chat/mod-chat.component.ts
+++ src/app/mod-chat/mod-chat.component.ts
@@ -1,78 +1,81 @@
-import {Component, ElementRef, EventEmitter, Input, Output, OnInit, ViewChild} from '@angular/core';
-import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
-import {Observable} from 'rxjs/Observable';
-
-import * as jQuery from 'jQuery';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import * as jQuery from 'jquery';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
-    selector: 'app-mod-chat',
-    templateUrl: './mod-chat.component.html',
-    styleUrls: ['./mod-chat.component.css']
+  selector: 'app-mod-chat',
+  templateUrl: './mod-chat.component.html',
+  styleUrls: ['./mod-chat.component.css']
 })
 export class ModChatComponent implements OnInit {
-    @ViewChild('chatScroll') private myScrollContainer: ElementRef;
+  @ViewChild('chatScroll') private myScrollContainer: ElementRef;
 
-    @Output() messageEventEmitter = new EventEmitter();
+  @Output() messageEventEmitter = new EventEmitter();
 
-    @Input() modToolsId = '';
-    @Input() username = '';
-    @Input() userRole = 'MOD';
+  @Input() modToolsId = '';
+  @Input() username = '';
+  @Input() userRole = 'MOD';
 
-    private chatMessagesRef: AngularFireList<any>;
-    private chatMessages: Observable<any[]>;
-    private msgVal = '';
-    private showNewMessageText = false;
+  private chatMessagesRef: AngularFireList<any>;
+  private chatMessages: Observable<any[]>;
+  private msgVal = '';
+  private showNewMessageText = false;
 
-    constructor(private afdb: AngularFireDatabase) {
+  constructor(private afdb: AngularFireDatabase) {}
+
+  ngOnInit() {
+    this.chatMessagesRef = this.afdb.list(
+      '/mod_tools/' + this.modToolsId + '/messages'
+    );
+    this.chatMessages = this.chatMessagesRef.valueChanges();
+    this.chatMessages.subscribe(updatedMessages => {
+      this.messageEventEmitter.emit(updatedMessages);
+
+      setTimeout(() => {
+        this.scrollToBottom();
+      }, 250);
+
+      if (jQuery('#collapseOne').attr('aria-expanded') === 'false') {
+        this.showNewMessageText = true;
+      }
+    });
+  }
+
+  private scrollToBottom() {
+    let panel: any = document.getElementsByName('chatBody')[0];
+
+    if (panel) {
+      panel.scrollTop = panel.scrollHeight;
     }
+  }
 
-    ngOnInit() {
-        this.chatMessagesRef = this.afdb.list('/mod_tools/' + this.modToolsId + '/messages');
-        this.chatMessages = this.chatMessagesRef.valueChanges();
-        this.chatMessages.subscribe(updatedMessages => {
-            this.messageEventEmitter.emit(updatedMessages);
-
-            setTimeout(() => {
-                this.scrollToBottom();
-            }, 250);
-
-            if (jQuery('#collapseOne').attr('aria-expanded') === 'false') {
-                this.showNewMessageText = true;
-            }
-        });
+  private chatSend(message: string) {
+    if (message) {
+      this.chatMessagesRef.push({
+        message: message,
+        username: this.username,
+        userRole: this.userRole,
+        timeSent: Date.now()
+      });
+      this.msgVal = '';
     }
+  }
 
-    private scrollToBottom() {
-        let panel: any = document.getElementsByName('chatBody')[0];
-
-        if (panel) {
-            panel.scrollTop = panel.scrollHeight;
-        }
+  private updateUsername(updatedUsername: string) {
+    if (updatedUsername) {
+      this.username = updatedUsername;
+      setTimeout(() => {
+        this.scrollToBottom();
+      }, 250);
     }
+  }
 
-    private chatSend(message: string) {
-        if (message) {
-            this.chatMessagesRef.push({message: message, username: this.username, userRole: this.userRole, timeSent: Date.now()});
-            this.msgVal = '';
-        }
-    }
+  private collapse() {
+    this.showNewMessageText = false;
 
-    private updateUsername(updatedUsername: string) {
-        if (updatedUsername) {
-            this.username = updatedUsername;
-            setTimeout(() => {
-                this.scrollToBottom();
-            }, 250);
-        }
-    }
-
-    private collapse() {
-        this.showNewMessageText = false;
-
-        setTimeout(() => {
-            this.scrollToBottom();
-        }, 250);
-
-    }
-
+    setTimeout(() => {
+      this.scrollToBottom();
+    }, 250);
+  }
 }
