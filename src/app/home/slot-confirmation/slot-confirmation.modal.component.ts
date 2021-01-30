55a3834544fc78890cdff7adf3711e8283ff58dd
jjmerri <jjmerri88@gmail.com>
2021-01-29 22:12:39 -0500

    change lego confirmation message -jjm

--- src/app/home/slot-confirmation/slot-confirmation.modal.component.ts
+++ src/app/home/slot-confirmation/slot-confirmation.modal.component.ts
@@ -81,6 +81,9 @@ export class SlotConfirmationModalComponent
           match = userMentionRegex.exec(this.context.comment.data.body);
         }
         if (this.context.numOpenSlots > 0 || this.context.inOrderMode) {
+          this.confirmationMessageText += ['lego_raffles'].includes(this.context.comment.data.subreddit)
+            ? '\n\n**Do not put any comments in your Paypal message or you will be permanently banned.**'
+            : '';
           this.confirmationMessageText +=
             '\n\nIf you do not receive an automated PM from me then you can confirm your payment by filling in and sending [this PM]({PAYMENT_MESSAGE_LINK}).';
           this.confirmationMessageText +=
