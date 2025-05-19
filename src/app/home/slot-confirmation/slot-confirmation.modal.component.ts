475ad4820e9ec6da0cccb634bea081c8fd736e8f
dasxce <dasxce@gmail.com>
2025-05-19 01:23:53 -0400

    Adding discord link to spot confirmation message

--- src/app/home/slot-confirmation/slot-confirmation.modal.component.ts
+++ src/app/home/slot-confirmation/slot-confirmation.modal.component.ts
@@ -147,9 +147,11 @@ export class SlotConfirmationModalComponent
             : '';
           if (!['FiftyFiftyToken'].includes(this.context.comment.data.subreddit)) {
             this.confirmationMessageText +=
-              '\n\nIf you do not receive an automated PM from me,  then you can confirm your payment **only after you have paid** by filling in and sending [this PM]({PAYMENT_MESSAGE_LINK}).';
-            this.confirmationMessageText +=
-              "\n\nIf the above link doesn't work then try [this one]({IOS_PAYMENT_MESSAGE_LINK}).";
+              '\n\nIf you do not receive an automated PM from me,  then you can confirm your payment **only after you have paid** by filling in and sending [this PM]({PAYMENT_MESSAGE_LINK}). If the prior link doesn't work then try [this one]({IOS_PAYMENT_MESSAGE_LINK}).';
+            this.confirmationMessageText += ['lego_raffles'].includes(this.context.comment.data.subreddit)
+              ? '\n\nThe Lego_Raffles subreddit has a Discord! Join us using this [link](https://discord.gg/legoraffles) to chat, trade, and play some mini-games!'
+            this.confirmationMessageText += ['PokemonRaffles'].includes(this.context.comment.data.subreddit)
+	      ? '\n\nThe PokemonRaffles subreddit has a Discord! Join us using this [link](https://discord.gg/JW8z4BpMga) to chat, trade, and play some mini-games!'
           }
         }
       } else {
